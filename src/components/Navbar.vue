5d71c443c3cf9e22660492c9a3ad3f5be742fab4
FT <trujillo.felipe1997@gmail.com>
2023-02-27 22:35:10 -0300

    tailwind

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="flex flex-row bg-gray-500">
+        <div class="basis-1/4 m-6">
+            <a href="#" class="no-underline hover:underline font-sans text-xl text-white font-medium">
+                Home
+            </a>
+        </div>
+        <div class="basis-1/4 m-6">
+            <a href="#" class="no-underline hover:underline font-sans text-xl text-white font-medium">
+                About
+            </a>
+        </div>
+        <div class="basis-1/4 m-6">
+            <a href="#" class="no-underline hover:underline font-sans text-xl text-white font-medium">
+                Skills
+            </a>
+        </div>
+
+        <div class="basis-1/2 m-6">
+            <div class="flex flex-row">
+
+                <div class="w-80">
+    
+                </div>
+    
+                <div class="w-10">
+                    <a href="#" class="no-underline hover:underline font-sans text-xl text-white font-medium">
+                        Inicio
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
---
+++ src/views/LandingView.vue
@@ -0,0 +1,17 @@
+<template>
+<Navbar/>
+
+<div class="w-100 bg-gray-800 h-24">
+
+    Test
+</div>
+<div>
+    <button class="rounded-md bg-purple-900 text-white p-3 font-sans font-medium text-sm">Contáctame</button>
+</div>
+</template>
+
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue';
+
+
+</script>
